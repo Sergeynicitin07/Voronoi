0dfcffc7a35f52a2c4517aabf4c685d2b38b4430
Сергей Никитин <115070040+Sergeynicitin07@users.noreply.github.com>
2026-07-23 17:07:10 +0300

    dcel.cpp

--- dcel.cpp
+++ dcel.cpp
@@ -1,39 +1,32 @@
 #include "dcel.h"
-
+#include <cmath>
 
 
 double pseudoscalar(const Vertex& a, const Vertex& b) {
     return (a.x * b.y) - (a.y * b.x);
 }
 
-
 // точки на плоскости a, b, c
 // < 0 => поворот направо
 // > 0 => поворот налево
 // = 0 => прямо
 
-
-
 const double EPS = 1e-9;
-
-
 
 // to find delanay centre
 Vertex we_should_find_centre(const Vertex& a, const Vertex& b, const Vertex& c) {
     double D = 2 * (a.x * (b.y - c.y) + b.x * (c.y - a.y) + c.x * (a.y - b.y));
 
-
     if (std::abs(D) < EPS) {
         return {(a.x + b.x + c.x) / 3.0, (a.y + b.y + c.y) / 3.0, -1};
     }
 
-    double ux = ((a.x * a.x + a.y * a.y) * (b.y - c.y) +
-                 (b.x * b.x + b.y * b.y) * (c.y - a.y) +
-                 (c.x * c.x + c.y * c.y) * (a.y - b.y)) / D;
+    double sa = a.x * a.x + a.y * a.y;
+    double sb = b.x * b.x + b.y * b.y;
+    double sc = c.x * c.x + c.y * c.y;
 
-    double uy = ((a.x * a.x + a.y * a.y) * (c.x - b.x) +
-                 (b.x * b.x + b.y * b.y) * (a.x - c.x) +
-                 (c.x * c.x + c.y * c.y) * (b.x - a.x)) / D;
+    double ux = (sa * (b.y - c.y) + sb * (c.y - a.y) + sc * (a.y - b.y)) / D;
+    double uy = (sa * (c.x - b.x) + sb * (a.x - c.x) + sc * (b.x - a.x)) / D;
 
     return {ux, uy, -1};
 }
@@ -46,12 +39,11 @@ int orientation (const Vertex& a, const Vertex& b, const Vertex& c) {
     Vertex ac = {c.x - a.x, c.y - a.y};
 
     // если больше нуля, то движение против часовой стрелки
-    if (pseudoscalar(ab, ac) > 0) return 1;
-    if (pseudoscalar(ab, ac) < 0) return -1;
+    double ps = pseudoscalar(ab, ac);
+    if (ps > EPS) return 1;
+    if (ps < -EPS) return -1;
     return 0;
-
 }
-
 
 bool convex (const Vertex& a, const Vertex& b, const Vertex& c) {
     return orientation(a, b, c) >= 0;
@@ -59,40 +51,27 @@ bool convex (const Vertex& a, const Vertex& b, const Vertex& c) {
 
 
 
-double det3x3(double m00, double m01, double m02,
-              double m10, double m11, double m12,
-              double m20, double m21, double m22) {
-    return m00 * (m11 * m22 - m12 * m21)
-           - m01 * (m10 * m22 - m12 * m20)
-           + m02 * (m10 * m21 - m11 * m20);
-}
-
-
 bool point_in_circle(const Vertex& a, const Vertex& b, const Vertex& c, const Vertex& p) {
-    double sa = a.x * a.x + a.y * a.y;
-    double sb = b.x * b.x + b.y * b.y;
-    double sc = c.x * c.x + c.y * c.y;
-    double sp = p.x * p.x + p.y * p.y;
+    long double adx = (long double)a.x - p.x;
+    long double ady = (long double)a.y - p.y;
+    long double bdx = (long double)b.x - p.x;
+    long double bdy = (long double)b.y - p.y;
+    long double cdx = (long double)c.x - p.x;
+    long double cdy = (long double)c.y - p.y;
 
-    double m14 = det3x3(sb, b.x, b.y,
-                        sc, c.x, c.y,
-                        sp, p.x, p.y);
+    long double ab = adx * adx + ady * ady;
+    long double bb = bdx * bdx + bdy * bdy;
+    long double cb = cdx * cdx + cdy * cdy;
 
-    double m24 = det3x3(sa, a.x, a.y,
-                        sc, c.x, c.y,
-                        sp, p.x, p.y);
+    long double det = ab * (bdx * cdy - bdy * cdx)
+                      - bb * (adx * cdy - ady * cdx)
+                      + cb * (adx * bdy - ady * bdx);
 
-    double m34 = det3x3(sa, a.x, a.y,
-                        sb, b.x, b.y,
-                        sp, p.x, p.y);
+    int ori = orientation(a, b, c);
+    if (ori == 0) return false;
 
-    double m44 = det3x3(sa, a.x, a.y,
-                        sb, b.x, b.y,
-                        sc, c.x, c.y);
-
-    double det = -m14 + m24 - m34 + m44;
-
-    return det > 0;
+    if (ori > 0) return det > 1e-9;
+    return det < -1e-9;
 }
 
 
@@ -112,10 +91,12 @@ std::vector<Vertex> DCEL::generateRandomDOTS(int n, int width, int height) {
     return dots;
 }
 
+
+
 void DCEL::bewilder () {
-    int dot0 = addVertex(-10000, -10000);
-    int dot1 = addVertex(10000, -10000);
-    int dot2 = addVertex(0, 10000);
+    int dot0 = addVertex(-1e7, -1e7);
+    int dot1 = addVertex(1e7, -1e7);
+    int dot2 = addVertex(0, 1e7);
 
     // это индекс для первой грани
     int f0 = 0;
@@ -134,11 +115,7 @@ void DCEL::bewilder () {
     edges.push_back({dot1, 0, 5,4, f0_});
     edges.push_back({dot2, 1,3,5, f0_});
     edges.push_back({dot0, 2,4,3, f0_});
-
-
 }
-
-
 
 int DCEL::locate(const Vertex& p) {
     if (faces.empty()) return -1;
@@ -170,8 +147,6 @@ int DCEL::locate(const Vertex& p) {
     return current;
 }
 
-
-
 std::vector<int> DCEL::stun (int face_idx, double x, double y) {
     // грань, в которую попала точка, мертва
     faces[face_idx].isdead = true;
@@ -185,8 +160,8 @@ std::vector<int> DCEL::stun (int face_idx, double x, double y) {
     int a = edges[edge0].origin; // Вершина A
     int b = edges[edge1].origin; // Вершина B
     int c = edges[edge2].origin; // Вершина C
+
     // создаем новые грани. в качестве индексов возьмем длину массива на данный момент
-    // При создании новых граней g0, g1, g2 сразу фиксируем их вершины
     int g0 = faces.size();
     faces.push_back({edge0, false, {}, {a, b, p}});
 
@@ -197,13 +172,13 @@ std::vector<int> DCEL::stun (int face_idx, double x, double y) {
     faces.push_back({edge2, false, {}, {c, a, p}});
     faces[face_idx].children = {g0, g1, g2};
 
-
     int edge_pa = edges.size();
     int edge_ap = edges.size() + 1;
     int edge_pb = edges.size() + 2;
     int edge_bp = edges.size() + 3;
     int edge_pc = edges.size() + 4;
     int edge_cp = edges.size() + 5;
+
     edges.push_back({p, edge_ap, edge0, edge_bp, g0});
     edges.push_back({a, edge_pa, edge_pc, edge2, g2});
     edges.push_back({p, edge_bp, edge1,  edge_cp, g1});
@@ -211,29 +186,20 @@ std::vector<int> DCEL::stun (int face_idx, double x, double y) {
     edges.push_back({p, edge_cp, edge2,  edge_ap, g2});
     edges.push_back({c, edge_pc, edge_pb, edge1, g1});
 
-
-
-
     edges[edge0].origin = a;
     edges[edge0].next = edge_bp;
     edges[edge0].prev = edge_pa;
     edges[edge0].face = g0;
-
-
 
     edges[edge1].origin = b;
     edges[edge1].next = edge_cp;
     edges[edge1].prev = edge_pb;
     edges[edge1].face = g1;
 
-
-
-
     edges[edge2].origin = c;
     edges[edge2].next = edge_ap;
     edges[edge2].prev = edge_pc;
     edges[edge2].face = g2;
-
 
     // точка p должна быть привязана к какому-то полуребру
     vertices[p].incident_edge = edge_pa;
@@ -241,9 +207,7 @@ std::vector<int> DCEL::stun (int face_idx, double x, double y) {
     return {edge0, edge1, edge2};
 }
 
-
 void DCEL::change_edge (int edge_ab){
-
     int edge_ba = edges[edge_ab].twin;
     int edge_bc = edges[edge_ab].next;
     int edge_ca = edges[edge_ab].prev;
@@ -255,14 +219,12 @@ void DCEL::change_edge (int edge_ab){
     int c = edges[edge_ca].origin;
     int d = edges[edge_db].origin;
 
-
     // выпишим грани
     int f_a = edges[edge_ab].face;
     int f_b = edges[edge_ba].face;
 
     faces[f_a].isdead = true;
     faces[f_b].isdead = true;
-
 
     int f_newA = faces.size();
     faces.push_back({edge_ab, false, {}, {c, d, b}}); // Треугольник P - D - B
@@ -273,16 +235,13 @@ void DCEL::change_edge (int edge_ab){
     faces[f_a].children = {f_newA, f_newB};
     faces[f_b].children = {f_newA, f_newB};
 
-
     // теперь точка A стала C, а точка B - D
     edges[edge_ab].origin = c;
     edges[edge_ba].origin = d;
 
-
     edges[edge_ab].face = f_newA;
     edges[edge_ab].next = edge_db;
     edges[edge_ab].prev = edge_bc;
-
 
     edges[edge_ba].face = f_newB;
     edges[edge_ba].next = edge_ca;
@@ -292,26 +251,17 @@ void DCEL::change_edge (int edge_ab){
     edges[edge_db].prev = edge_ab;
     edges[edge_db].face = f_newA;
 
-
     edges[edge_bc].face = f_newA;
     edges[edge_bc].next = edge_ab;
     edges[edge_bc].prev = edge_db;
-
-
-    edges[edge_ba].face = f_newB;
-    edges[edge_ba].next = edge_ca;
-    edges[edge_ba].prev = edge_ad;
-
 
     edges[edge_ca].face = f_newB;
     edges[edge_ca].next = edge_ad;
     edges[edge_ca].prev = edge_ba;
 
-
     edges[edge_ad].face = f_newB;
-    edges[edge_ad].prev = edge_ca;
     edges[edge_ad].next = edge_ba;
-
+    edges[edge_ad].prev = edge_ca;
 
     faces[f_newA].inner_comp = edge_ab;
     faces[f_newB].inner_comp = edge_ba;
@@ -320,15 +270,7 @@ void DCEL::change_edge (int edge_ab){
     vertices[b].incident_edge = edge_bc;
     vertices[c].incident_edge = edge_ab;
     vertices[d].incident_edge = edge_ba;
-
-
-
-
-
-
 }
-
-
 
 void DCEL::manage (int p0, int holy_edge) {
     int twinki_pinki = edges[holy_edge].twin;
@@ -344,29 +286,20 @@ void DCEL::manage (int p0, int holy_edge) {
     Vertex p = vertices[p0];
     Vertex d = vertices[d0];
 
-
     if (point_in_circle(b, a, d, p)) {
-
-
         int pr = edges[twinki_pinki].prev;
         int nx = edges[twinki_pinki].next;
         change_edge(holy_edge);
         // сделаем рекурсию для соседей грани и точки p
         manage(p0, pr);
         manage(p0, nx);
-
     }
 }
 
-
-
 void DCEL::turn_into (double x, double y) {
-
-
     Vertex dot = {x, y, -1};
     int face_x = locate(dot);
     if (face_x == -1) return;
-
 
     std::vector<int> triangle_ = stun(face_x, x, y);
     // в шоковой функции мы уже добавили p в список
@@ -374,5 +307,5 @@ void DCEL::turn_into (double x, double y) {
     for (int edge_h : triangle_) {
         manage(p_, edge_h);
     }
-
 }
+
