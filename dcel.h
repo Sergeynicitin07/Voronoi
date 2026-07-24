9368fb6c0c545d59f82242fa66728bf6599dfa20
Сергей Никитин <115070040+Sergeynicitin07@users.noreply.github.com>
2026-07-25 01:32:49 +0300

    dcel.h

--- dcel.h
+++ dcel.h
@@ -4,8 +4,11 @@
 #include <cmath>
 #include <cstdlib>
 #include <algorithm>
+#include <cmath>
 #include <random>
 
+
+// структура DCEL
 struct Vertex {
     double x, y;
     int incident_edge = -1;
@@ -20,10 +23,9 @@ struct HalfEdge {
 };
 
 struct Face {
+    // одно ребро
     int inner_comp = -1;
-    bool isdead = false;
-    std::vector<int> children;
-    int v[3] = {-1, -1, -1};
+
 };
 
 class DCEL {
@@ -34,21 +36,45 @@ public:
 
     int addVertex(double x, double y);
     void bewilder();
-    int locate(const Vertex& p);
     std::vector<int> stun(int face_idx, double x, double y);
     void change_edge(int edge_ab);
-    void manage(int p0, int holy_edge);
-    void turn_into(double x, double y);
-    std::vector<Vertex> generateRandomDOTS(int n, int width, int height);
-    void build_triangulation(std::vector<Vertex>& points);
 
 };
+
+
+struct History_Face {
+    bool isdead = false;
+    std::vector<int> children;
+    int v[3] = {-1, -1, -1};
+    // индекс грани
+    int face0 = -1;
+};
+
+
+
+
+class Delaunay {
+public:
+    DCEL dcel;
+    std::vector<History_Face> history;
+
+    int locate(const Vertex& p);
+    void manage(int p0, int holy_edge);
+    void turn_into(double x, double y);
+
+
+};
+
+
+
+std::vector<Vertex> generateRandomDOTS(int n, int width, int height);
 double det3x3(double m00, double m01, double m02,
               double m10, double m11, double m12,
               double m20, double m21, double m22);
+Vertex we_should_find_centre(const Vertex& a, const Vertex& b, const Vertex& c);
 double pseudoscalar(const Vertex& a, const Vertex& b);
 int orientation(const Vertex& a, const Vertex& b, const Vertex& c);
 bool convex(const Vertex& a, const Vertex& b, const Vertex& c);
-Vertex we_should_find_centre(const Vertex& a, const Vertex& b, const Vertex& c);
 bool point_in_circle(const Vertex& a, const Vertex& b, const Vertex& c, const Vertex& p);
+
 #endif //UNTITLED41_DCEL_H
