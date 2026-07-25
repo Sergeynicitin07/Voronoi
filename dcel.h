da285c37d83430be12c337043ad89b775f486475
Сергей Никитин <115070040+Sergeynicitin07@users.noreply.github.com>
2026-07-25 22:12:35 +0300

    dcel.h

--- dcel.h
+++ dcel.h
@@ -4,11 +4,9 @@
 #include <cmath>
 #include <cstdlib>
 #include <algorithm>
-#include <cmath>
 #include <random>
 
 
-// структура DCEL
 struct Vertex {
     double x, y;
     int incident_edge = -1;
@@ -22,11 +20,12 @@ struct HalfEdge {
     int face = -1;
 };
 
-struct Face {
-    // одно ребро
-    int inner_comp = -1;
 
+struct Face {
+    int inner_comp = -1;
 };
+
+
 
 class DCEL {
 public:
@@ -38,43 +37,42 @@ public:
     void bewilder();
     std::vector<int> stun(int face_idx, double x, double y);
     void change_edge(int edge_ab);
-
 };
-
-
-struct History_Face {
-    bool isdead = false;
-    std::vector<int> children;
-    int v[3] = {-1, -1, -1};
-    // индекс грани
-    int face0 = -1;
-};
-
 
 
 
 class Delaunay {
 public:
     DCEL dcel;
-    std::vector<History_Face> history;
+    bool is_finalized = false;
 
-    int locate(const Vertex& p);
-    void manage(int p0, int holy_edge);
     void turn_into(double x, double y);
 
+    void finalize();
 
+private:
+    int locate(const Vertex& p);
+    void manage(int p0, int holy_edge);
+
+    struct History_Face {
+        bool isdead = false;
+        std::vector<int> children;
+        int v[3] = {-1, -1, -1};
+        int face0 = -1;
+    };
+
+    std::vector<History_Face> history;
 };
 
-
-
 std::vector<Vertex> generateRandomDOTS(int n, int width, int height);
+Vertex we_should_find_centre(const Vertex& a, const Vertex& b, const Vertex& c);
 double det3x3(double m00, double m01, double m02,
               double m10, double m11, double m12,
               double m20, double m21, double m22);
-Vertex we_should_find_centre(const Vertex& a, const Vertex& b, const Vertex& c);
 double pseudoscalar(const Vertex& a, const Vertex& b);
 int orientation(const Vertex& a, const Vertex& b, const Vertex& c);
 bool convex(const Vertex& a, const Vertex& b, const Vertex& c);
 bool point_in_circle(const Vertex& a, const Vertex& b, const Vertex& c, const Vertex& p);
 
 #endif //UNTITLED41_DCEL_H
+
